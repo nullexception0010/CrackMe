571fb85f2a4a11856ccfcba54cf428691b4f0a20
null_exception <jpsutka3672@conestogac.on.ca>
2024-01-12 18:14:56 -0500

    first commit

---
+++ CrackMe/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+int main() {
+    const std::string password = "SET2024";
+    std::string guess;
+
+    std::cout << "Enter Password: ";
+    std::getline(std::cin, guess);
+
+    if (password == guess) {
+        std::cout << "Success!\n";
+    }
+    else {
+        std::cout << "Passwords Don't Match!\n";
+    }
+
+    std::cout << "\nPress any key to close...";
+    std::cin.get();
+
+    return 0;
+}
